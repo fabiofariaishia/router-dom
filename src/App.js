36dda0a701e69c85a3c33e2961d9ec49ec249bb4
Fabio Faria <fhabyomontilla@gmail.com>
2024-06-05 22:19:23 -0300

    Incluído rotas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Inicio from './paginas/Inicio';
 import SobreMim from './paginas/SobreMim';
 
 console.log(window.location);
 
-const pagina = window.location.pathname === '/' ? <Inicio /> : <SobreMim />;
-
 function App() {
     return (
         <BrowserRouter>
-            
+            <Routes>
+                <Route path="/" element={<Inicio />} />
+                <Route path="/sobremim" element={<SobreMim />} />
+                <Route path='*' element={<h1>Página não encontrada.</h1>} />
+            </Routes>
         </BrowserRouter>
     );
 }
